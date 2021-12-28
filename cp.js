2817905135abacd67ba8a2330f07f6fca96ab3bb
guoxx03 <guoxx03@fenbi.com>
2021-12-28 08:33:21 +0800

    # 这是一个 4 个提交的组合。 # 这是第一个提交说明：
    MOD
    
    # 这是提交说明 #2：
    
    MOD: cp.js
    
    # 这是提交说明 #3：
    
    MOD: 修改cp.js
    
    # 这是提交说明 #4：
    
    MOD: 修改cp.js

--- cp.js
+++ cp.js
@@ -2,5 +2,12 @@ const path = require("path");
 const fs = require("fs");
 const originUrl = path.join(__dirname, "./docs/快速导航.md");
 const targetUrl = path.join(__dirname, "./README.md");
-const content = fs.readFileSync(originUrl, "utf-8");
-fs.writeFileSync(targetUrl, content);
+try {
+  const content = fs.readFileSync(originUrl, "utf-8");
+  fs.writeFileSync(targetUrl, content);
+  console.log("README.md 写入完成✅");
+  process.exit(0);
+} catch (e) {
+  console.log(e, "README.md写入失败❌");
+  process.exit(1);
+}
